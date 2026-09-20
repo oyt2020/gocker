3115d1645a12ef8e8331573a9c8ba34b843e2c40
oyt2020 <oyt201@naver.com>
2026-09-20 11:31:48 +0900

    gocker start 1차 테스트

--- main.go
+++ main.go
@@ -63,6 +63,8 @@ func main() {
 	case "stop":
 		handleStopCommand(runtime, os.Args[2:])
 		//handleStop(runtime, "default", 10, []string{"f9401a0c54b7"})
+	case "start":
+		handleStart(runtime, "default", false, []string{"my_nginx"})
 	default:
 		fmt.Printf("Unknown command: %s\n", os.Args[1])
 		os.Exit(1)
